744f70db17b2251ccc55ae3242a354bd88967135
senali <senalidilumika@gmail.com>
2022-10-12 23:31:07 +0530

    Update favicon

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -16,7 +16,9 @@ const Navbar = () => {
           <span className="self-center text-[14px] uppercase font-semibold whitespace-nowrap text-white">Mini Encyclopedia</span>
       </a>
       <div className="flex md:order-2">
-          <button type="button" className="text-white bg-[#f99839] hover:bg-[#f3810f] focus:ring-4 focus:outline-none focus:ring-[#f99839] font-semibold rounded-lg text-sm px-2.5 py-2.5 text-center mr-3 md:mr-0"><AiFillGithub size={20} /></button>
+          <a target="_blank" href="https://github.com/senali-d/mini-encyclopedia">
+            <button type="button" className="text-white bg-[#f99839] hover:bg-[#f3810f] focus:ring-4 focus:outline-none focus:ring-[#f99839] font-semibold rounded-lg text-sm px-2.5 py-2.5 text-center mr-3 md:mr-0"><AiFillGithub size={20} /></button>
+          </a>
           <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg bg-gray-100 md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" onClick={handleClick}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
