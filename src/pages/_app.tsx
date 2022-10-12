a84f1ca6b7c0c4a405ac3959d1be7f22e12973c3
senali <senalidilumika@gmail.com>
2022-10-12 23:00:43 +0530

    Nhost initialize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NhostNextProvider, NhostClient } from '@nhost/nextjs'
+
+import '../styles/globals.css'
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || ''
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <Component {...pageProps} />
+    </NhostNextProvider>
+  )
 }
 
 export default MyApp
