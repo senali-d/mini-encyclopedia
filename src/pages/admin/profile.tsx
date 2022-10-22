c4090a9ac3d6659777f7881bd6146d6435546cba
senali <senalidilumika@gmail.com>
2022-10-22 16:31:47 +0530

    fix: deploy issue

--- src/pages/admin/profile.tsx
+++ src/pages/admin/profile.tsx
@@ -200,8 +200,8 @@ const ProfileTable = ({refetchData}: any) => {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              {profileTable.map((profile) => (
-                <th scope="col" className="py-3 px-6">
+              {profileTable.map((profile, index) => (
+                <th scope="col" className="py-3 px-6" key={index}>
                   {profile.label}
                 </th>
               ))}
@@ -209,7 +209,7 @@ const ProfileTable = ({refetchData}: any) => {
           </thead>
           <tbody>
             {profile.map((p: any) => (
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={p.id}>
                 <th
                   scope="row"
                   className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
